5afdbc12e84c7572e69c70049d4da40f840dd638
Ramon Sifuentes <6459758+rdavidsc@users.noreply.github.com>
2025-07-09 15:17:09 -0500

    v1.5.2 - Bug Fixed scaping plan text when updating fields

--- src/index.ts
+++ src/index.ts
@@ -87,11 +87,11 @@ export class PipefyAPI {
   }
 
   makeComment(cardId: string, text: string): Promise<Response>{
-    return this.pipefyFetch(`mutation{ createComment(input:{ card_id: "${cardId}", text: "${text.replace(/"/g,"")}" }) { clientMutationId } }`)
+    return this.pipefyFetch(`mutation{ createComment(input:{ card_id: "${cardId}", text: "${text.replaceAll(/"/g, "\\\"")}" }) { clientMutationId } }`)
   }
 
   updateFaseField(cardId: string, name: string, value: any, valueIsArray = false ): Promise<Response>{
-    let valueTosend = `"${value}"`
+    let valueTosend = `"${value.replaceAll(/"/g, "\\\"")}"`
     if(valueIsArray || Array.isArray(value) ){
       valueTosend = `[ "${value.join('", "')}" ]`
     }
@@ -117,7 +117,7 @@ export class PipefyAPI {
         }
       } else {
         if(fieldsToUpdate[field] != null || fieldsToUpdate[field] != undefined){
-          fieldArray.push(`{fieldId: "${field}", value: "${fieldsToUpdate[field]}" }`)
+          fieldArray.push(`{fieldId: "${field}", value: "${fieldsToUpdate[field].replaceAll(/"/g, "\\\"")}" }`)
         } else if(fieldsToUpdate[field] == null) {
           fieldArray.push(`{fieldId: "${field}", value: null }`)
         } else if(fieldsToUpdate[field] == undefined) {
@@ -377,7 +377,7 @@ export class PipefyAPI {
   
     } else {
       if(value != null || value != undefined){
-        return `{ field_id: "${fieldName}", field_value: "${value}" }`
+        return `{ field_id: "${fieldName}", field_value: "${value.replaceAll(/"/g, "\\\"")}" }`
       } else {
         // console.log("FIELD SKIPPED:",fieldName)
         return null
